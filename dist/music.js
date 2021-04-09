4581feaf7d2ffac0cc56d02c0bc80913b04188e8
Zihang-Xie <fqxzh123456@163.com>
2021-04-09 22:06:13 +0800

    Site updated: 2021-04-09 22:06:07

--- dist/music.js
+++ dist/music.js
@@ -1,13 +1,17 @@
 const ap = new APlayer({
-    container: document.getElementById('aplayer'), //播放器容器元素
-    fixed: true,
-    autoplay: false;
-    audio: [  //音频信息,包含以下
-        {
-            name: '大风吹', //音频名称
-            artist: '王赫野', //音频艺术家
-            url: '/music/music1.m4a', //音频外链
-            cover: '/music/music1.jpg', //音频封面
-        },
-    ]
+  container: document.getElementById('aplayer'), //播放器容器元素
+  loop: 'all',
+  volume: 0.7,
+  listFolded: true,
+  listMaxHeight: 60,
+  fixed: true,
+  autoplay: false,
+  audio: [
+    {  				        //音频信息,包含以下
+      name: '大风吹', 			//音频名称
+      artist: '王赫野', 		        //音频艺术家
+      url: '/music/music1.m4a', 	//音频外链
+      cover: '/music/music1.jpg', 	//音频封面
+      }
+  ]
 });
